135cc71983acc2523d2510719668c68bea7fa4b5
Nivhaham <49638534+Nivhaham@users.noreply.github.com>
2023-10-30 16:43:11 +0200

    done Intro

--- Intro-to-ML/main.py
+++ Intro-to-ML/main.py
@@ -1,6 +1,29 @@
 import os
 import pandas as pd
 from sklearn.tree import DecisionTreeRegressor
+from sklearn.metrics import mean_absolute_error
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error
+
+
+def validating_model(X, y):
+    train_X, val_X, train_y, val_y = train_test_split(X, y, random_state=0)
+    # Define model
+    melbourne_model = DecisionTreeRegressor()
+    # Fit model
+    melbourne_model.fit(train_X, train_y)
+
+    # get predicted prices on validation data
+    val_predictions = melbourne_model.predict(val_X)
+    print(mean_absolute_error(val_y, val_predictions))
+
+    # Forest model
+
+    forest_model = RandomForestRegressor(random_state=1)
+    forest_model.fit(train_X, train_y)
+    melb_preds = forest_model.predict(val_X)
+    print(mean_absolute_error(val_y, melb_preds))
 
 
 def main():
@@ -22,10 +45,9 @@ def main():
     # Fit model
     melbourne_model.fit(X, y)
 
-    print("Making predictions for the following 5 houses:")
-    print(X.head())
-    print("The predictions are")
-    print(melbourne_model.predict(X.head()))
+    # print("The predictions are")
+    # print(melbourne_model.predict(X.head()))
+    validating_model(X, y)
 
 
 if __name__ == '__main__':
