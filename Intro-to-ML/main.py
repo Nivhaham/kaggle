616dfee03368caf51499817e2177308f745700af
Nivhaham <49638534+Nivhaham@users.noreply.github.com>
2023-10-30 13:57:26 +0200

    master

--- Intro-to-ML/main.py
+++ Intro-to-ML/main.py
@@ -1,19 +1,32 @@
 import os
 import pandas as pd
+from sklearn.tree import DecisionTreeRegressor
+
 
 def main():
     melbourne_data = pd.read_csv("melb_data.csv")
-    #print(melbourne_data.describe())
-    #print(melbourne_data.columns)
+    # print(melbourne_data.describe())
+    # print(melbourne_data.columns)
 
     melbourne_data = melbourne_data.dropna(axis=0)
-    #y = melbourne_data.Price
-    #print(y)
+    y = melbourne_data.Price
+    # print(y)
 
     # Picking Subset of Columns
     melbourne_features = ['Rooms', 'Bathroom', 'Landsize', 'Lattitude', 'Longtitude']
     X = melbourne_data[melbourne_features]
     print(X.describe())
+
+    melbourne_model = DecisionTreeRegressor(random_state=1)
+
+    # Fit model
+    melbourne_model.fit(X, y)
+
+    print("Making predictions for the following 5 houses:")
+    print(X.head())
+    print("The predictions are")
+    print(melbourne_model.predict(X.head()))
+
 
 if __name__ == '__main__':
     main()
