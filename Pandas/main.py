a591cdeb1bf55295244d1227a331e3c62ce48e0f
Nivhaham <49638534+Nivhaham@users.noreply.github.com>
2023-11-01 10:03:07 +0200

    pandas atts

--- Pandas/main.py
+++ Pandas/main.py
@@ -4,6 +4,10 @@ import pandas as pd
 def main():
     ramen_df = pd.read_csv('ramen-ratings.csv')
     print(ramen_df)
+    print(ramen_df.Stars.describe())
+    # line 9, 10 same
+    #print(ramen_df.Stars.unique())
+    #print(ramen_df['Stars'].unique())
 
 
 if __name__ == '__main__':
